922b7d283bd81b65ee4106558137340cba1dfa68
Jeremiah Greer <JeremiahGreer013@gmail.com>
2018-01-08 21:34:10 -0500

    Heavily Refactored frozenlake.py

--- morl/examples/frozenlake.py
+++ morl/examples/frozenlake.py
@@ -1,152 +1,108 @@
-import gym
-from ..learning.sequential.qlearn import QLearn
+import matplotlib.pyplot as plt
 
-import sys
+from ..learning.sequential.qlearn import QLearn
 
 from ..learning.sequential.multilearn import MultiLearn
 
+import gym
 #from source.config import Config
 
-import re
-import matplotlib.pyplot as plt
-import math
+#import re
 
-#LEFT = 0
-#DOWN = 1
-#RIGHT = 2
-#UP = 3
+#import math
 
-env = gym.make('FrozenLake-v0')
-menv = gym.make('FrozenLake-v0')
-eps = 1.0
-gam = 0.95
-space = env.action_space
-acts = range(space.n)
-acts_dict = {}
-for s in range(env.observation_space.n):
-  acts_dict[s] = acts
-  
-def punish_falls(x):
-  if (x[2] == True and x[1] != 1.0):
-    return -10
-  elif (x[2] == True and x[1] == 1.0):
-    return 1.0
-  else:
-    return -1
-  
-num_epochs = 2000
-num_tests = 99
+_LEFT = 0
+_DOWN = 1
+_RIGHT = 2
+_UP = 3
 
-for trial in range(1, 10):
-    epoch_only_rewards = []
+ENV = gym.make('FrozenLake-v0')
+ACTS_DICT = {}
+ACTS = [_LEFT, _DOWN, _RIGHT, _UP]
+for s in range(ENV.observation_space.n):
+    ACTS_DICT[s] = ACTS
 
-    reward_per_epoch = []
+def punish_falls(reward_tuple):
+    if (reward_tuple[2] and reward_tuple[1] != 1.0):
+        return -10
+    elif (reward_tuple[2] and reward_tuple[1] == 1.0):
+        return 1.0
+    else:
+        return -1
 
-    prev_reward = 0
+def run_frozen_lake_qlearn(epsilon=1.0, gamma=0.95, alpha=0.4, 
+        reward_function=lambda x: x[1], num_epochs=2000, num_tests=99, 
+        increment_alpha=False, show_plots=False, plot_name_prefix="plot_learning_rate_"):
+    learner = QLearn(actions=ACTS_DICT, epsilon=epsilon, alpha=alpha, gamma=gamma, reward_function=reward_function)
+    run_frozen_lake(learner, num_epochs, num_tests, show_plots, plot_name_prefix, increment_alpha)
 
-    alph = trial / 10.0
-    q = QLearn(acts_dict, epsilon=eps, alpha=alph, gamma=gam, reward_function=lambda x: x[1])
-    reward_funcs = [lambda x: x[1], punish_falls ]
-    mq = MultiLearn(actions=acts_dict, epsilon=eps, alpha=alph, gamma=gam, reward_functions=reward_funcs)
-    for epoch in range(num_epochs):
-        epoch_reward = 0
-        mepoch_reward = 0
-        prev_observation = env.reset()
-        mprev_observation = menv.reset()
-        this_reward = 0
-        #env.render()
-        #q.epsilon = max(1 / (epoch + 1), 0.001)
-        # temperature for boltzmann
-        ep = max(1 / (epoch + 1), 0.01)
-        q.epsilon = ep
-        mq.epsilon = ep
-        #q.alpha = 1 / (epoch + 1)
-        # Learning 
-        q.train(start_state=prev_observation, environment=env, max_iter=99)
-        mq.train(start_state=prev_observation, environment=menv, max_iter=99)
+def run_frozen_lake_multilearn(epsilon=1.0, gamma=0.95, alpha=0.4, 
+        reward_functions=[lambda x: x[1], punish_falls], num_epochs=2000, num_tests=99, 
+        increment_alpha=False, show_plots=False, plot_name_prefix="plot_learning_rate_"):
+    learner = MultiLearn(actions=ACTS_DICT, epsilon=epsilon, alpha=alpha, gamma=gamma, reward_functions=reward_functions)
+    run_frozen_lake(learner, num_epochs, num_tests, show_plots, plot_name_prefix, increment_alpha)
+
+def run_frozen_lake(qlearner, num_epochs, num_tests, increment_alpha, show_plots, plot_name_prefix):
+    for trial in range(1, 10):
+        epoch_only_rewards = []
+
+        reward_per_epoch = []
+
+        if increment_alpha == True:
+            alph = trial / 10.0
         
-        
-        
-        """
-        for t in range(99):
-        
-            action = q.choose_action(prev_observation)
+        for epoch in range(num_epochs):
+            epoch_reward = 0
+            prev_observation = ENV.reset()
             
-            observation, reward, done, info = env.step(action)
+            #env.render()
+            #q.epsilon = max(1 / (epoch + 1), 0.001)
+            # temperature for boltzmann
+            eps = max(1 / (epoch + 1), 0.01)
+            qlearner.epsilon = eps
+            #q.alpha = 1 / (epoch + 1)
+            # Learning 
+            qlearner.train(start_state=prev_observation, environment=ENV, max_iter=99)
             
-            #epoch_reward += reward
-            
-            sars = (prev_observation, action, reward, observation)
-            
-            this_reward += reward
-            
-            q.learn(prev_observation, action, reward, observation)
-            #print sars #SARS aka Q-Learning
-            #print("action: ", action)
-            #print("observation: ", observation)
-            #print("reward: ", reward)
-            #print("done: ", done)
-            #print("info: ", info)
-            
-            prev_observation = observation
-            
-            if done:
-                #print("Episode finished after {} timesteps".format(t+1))
-                break
-        
-        epoch_only_rewards.append(this_reward)
-        """        
-        
-        # Testing
-        q.epsilon = 0.00 # temperature for boltzmann
-        mq.epsilon = 0.00
-        #q.epsilon = 0.00 # epsilon for other methods
-        for i in range(num_tests):
-            prev_observation = env.reset()
-            mprev_observation = menv.reset()
-            for t in range(100):
-                action = q.choose_action(prev_observation)
-                maction = mq.choose_action(mprev_observation)
-                
-                observation, reward, done, info = env.step(action)
-                mobservation, mreward, mdone, minfo = menv.step(maction)
-                
-                epoch_reward += reward
-                mepoch_reward += mreward
-                
-                sars = (prev_observation, action, reward, observation)
-                
-                msars = (mprev_observation, maction, mreward, mobservation)
-                
-                prev_observation = observation
-                mprev_observation = mobservation
-                
-                if done:
-                    #print("Episode finished after {} timesteps".format(t+1))
-                    break
+            # Testing
+            qlearner.epsilon = 0.00 # temperature for boltzmann
+            #q.epsilon = 0.00 # epsilon for other methods
+            for _i in range(num_tests):
+                prev_observation = ENV.reset()
+                for _t in range(100):
+                    action = qlearner.choose_action(prev_observation)
                     
-          
-        if (epoch % 10 == 0):          
-            print("Epoch {} finished, total reward: {}".format(epoch+1, epoch_reward))
-        
-        reward_per_epoch.append(epoch_reward/num_tests)    
-        
-        #print q.q  
-        
-           
+                    observation, reward, done, _ = ENV.step(action)
+                    
+                    epoch_reward += reward
+                    
+                    prev_observation = observation
+                    
+                    if done:
+                        break
+                        
+            
+            if (epoch % 10 == 0):          
+                print("Epoch {} finished, total reward: {}".format(epoch+1, epoch_reward))
+            
+            reward_per_epoch.append(epoch_reward/num_tests)
+            
+            
+        if show_plots:
+            plt.plot(range(num_epochs), reward_per_epoch)
+            #plt.show()
+            plotname = plot_name_prefix + "{0:.1f}.png"
+            plt.savefig(plotname.format(alph))
+            plt.cla()
+            plt.clf()
+            plt.close()
 
-    plt.plot(range(num_epochs), reward_per_epoch)
-    #plt.show()
-    plt.savefig("plot_learning_rate_{0:.1f}.png".format(alph))
-    plt.cla()
-    plt.clf()
-    plt.close()
-
-    print sum(reward_per_epoch) / num_epochs
-    print sum(epoch_only_rewards) / num_epochs
-    
-    break
+        print sum(reward_per_epoch) / num_epochs
+        print sum(epoch_only_rewards) / num_epochs
         
+        break
+        
+"""
 s = range(16)
 aa = range(4)
 a = {'1':['a','b'], '2':['c','d'], '3':['e','f']}
@@ -155,4 +111,5 @@ dest = {('1','a'): '2',('1','b'): '3',('2','c'): '1',('2','d'): '3',('3','e'): '
 df = 0.9
 r = {'1': -5,'2': 0,'3': 5}
 stop = 0.1
+"""
 
