28b23e4ab1d1e321429fe9af46c214c036bc48ab
Frazier Baker <frazbak@gmail.com>
2018-01-10 00:18:25 -0500

    Cleaned up a lot of code with the help of pylint

--- morl/__init__.py
+++ morl/__init__.py
@@ -1,6 +1,3 @@
-import sys
-# sys.path.append("../")
-
 from . import learning
 from . import config
 from . import examples
--- morl/config.py
+++ morl/config.py
@@ -1,5 +1,10 @@
 import os
 
 class Config(object):
+    """ Config contains configuration defaults and environment
+        variables to override.  It functions as a static class
+        with class variables.  Don't make instances or try to 
+        access config variablses as member variables.
+    """
     SPARK_MASTER = os.environ.get('SPARK_MASTER', 'local')
     SPARK_APP_NAME = os.environ.get('SPARK_APP_NAME', 'morl')
