5fa0e17ddce8ac361c242760db6260103f1b9e06
Frazier Baker <frazbak@gmail.com>
2018-01-15 12:35:05 -0500

    Moved environment definition for frozenlake into functions

--- morl/examples/frozenlake.py
+++ morl/examples/frozenlake.py
@@ -9,17 +9,6 @@ from ..learning.sequential.multilearn import MultiLearn
 
 #import math
 
-_LEFT = 0
-_DOWN = 1
-_RIGHT = 2
-_UP = 3
-
-ENV = gym.make('FrozenLake-v0')
-ACTS_DICT = {}
-ACTS = [_LEFT, _DOWN, _RIGHT, _UP]
-for s in range(ENV.observation_space.n):
-    ACTS_DICT[s] = ACTS
-
 def punish_falls(reward_tuple):
     if (reward_tuple[2] and reward_tuple[1] != 1.0):
         return -10
@@ -31,19 +20,40 @@ def run_frozen_lake_qlearn(epsilon=1.0, gamma=0.95, alpha=0.4,
                            reward_function=lambda x: x[1], num_epochs=2000,
                            num_tests=99, increment_alpha=False, show_plots=False,
                            plot_name_prefix="plot_learning_rate_"):
-    learner = QLearn(actions=ACTS_DICT, epsilon=epsilon, alpha=alpha, gamma=gamma,
+    _left = 0
+    _down = 1
+    _right = 2
+    _up = 3
+    acts_dict = {}
+    acts = [_left, _down, _right, _up]
+    env = gym.make('FrozenLake-v0')
+    for state in range(env.observation_space.n):
+        acts_dict[state] = acts
+    learner = QLearn(actions=acts_dict, epsilon=epsilon, alpha=alpha, gamma=gamma,
                      reward_function=reward_function)
-    run_frozen_lake(learner, num_epochs, num_tests, show_plots, plot_name_prefix, increment_alpha)
+    run_frozen_lake(learner, num_epochs, num_tests, show_plots, plot_name_prefix,
+                    increment_alpha, env)
 
 def run_frozen_lake_multilearn(epsilon=1.0, gamma=0.95, alpha=0.4,
                                reward_functions=(lambda x: x[1], punish_falls),
                                num_epochs=2000, num_tests=99, increment_alpha=False,
                                show_plots=False, plot_name_prefix="plot_learning_rate_"):
-    learner = MultiLearn(actions=ACTS_DICT, epsilon=epsilon, alpha=alpha, gamma=gamma,
+    _left = 0
+    _down = 1
+    _right = 2
+    _up = 3
+    acts_dict = {}
+    acts = [_left, _down, _right, _up]
+    env = gym.make('FrozenLake-v0')
+    for state in range(env.observation_space.n):
+        acts_dict[state] = acts
+    learner = MultiLearn(actions=acts_dict, epsilon=epsilon, alpha=alpha, gamma=gamma,
                          reward_functions=reward_functions)
-    run_frozen_lake(learner, num_epochs, num_tests, show_plots, plot_name_prefix, increment_alpha)
+    run_frozen_lake(learner, num_epochs, num_tests, show_plots, plot_name_prefix,
+                    increment_alpha, env)
 
-def run_frozen_lake(qlearner, num_epochs, num_tests, increment_alpha, show_plots, plot_name_prefix):
+def run_frozen_lake(qlearner, num_epochs, num_tests, increment_alpha, show_plots,
+                    plot_name_prefix, env):
     for trial in range(1, 10):
         epoch_only_rewards = []
 
@@ -54,7 +64,7 @@ def run_frozen_lake(qlearner, num_epochs, num_tests, increment_alpha, show_plots
 
         for epoch in range(num_epochs):
             epoch_reward = 0
-            prev_observation = ENV.reset()
+            prev_observation = env.reset()
 
             #env.render()
             #q.epsilon = max(1 / (epoch + 1), 0.001)
@@ -63,17 +73,17 @@ def run_frozen_lake(qlearner, num_epochs, num_tests, increment_alpha, show_plots
             qlearner.epsilon = eps
             #q.alpha = 1 / (epoch + 1)
             # Learning
-            qlearner.train(start_state=prev_observation, environment=ENV, max_iter=99)
+            qlearner.train(start_state=prev_observation, environment=env, max_iter=99)
 
             # Testing
             qlearner.epsilon = 0.00 # temperature for boltzmann
             #q.epsilon = 0.00 # epsilon for other methods
             for _i in range(num_tests):
-                prev_observation = ENV.reset()
+                prev_observation = env.reset()
                 for _t in range(100):
                     action = qlearner.choose_action(prev_observation)
 
-                    observation, reward, done, _ = ENV.step(action)
+                    observation, reward, done, _ = env.step(action)
 
                     epoch_reward += reward
 
